83d3572068681710383706541adbf6233723ac34
snitkdan <snitkdan@uw.edu>
2016-08-10 15:51:41 -0700

    Fix

--- ML7/views.py
+++ ML7/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_list_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import Stock
+from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 
@@ -33,28 +33,3 @@ class BookAuthList(APIView):
 
 
 
-
-
-
-from .models import Book
-from .serializers import BookAuthSerializer
-from rest_framework import generics
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
-
-
-class book_list(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookAuthSerializer
-
-
-class book_detail(generics.RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookAuthSerializer
-
-@api_view(('GET',))
-def root(request, format=None):
-    return Response({
-        'books': reverse('book-list', request=request, format=format),
-    })
