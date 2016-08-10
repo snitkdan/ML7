397f7ee518a37dfb793d7bbc2c16e7b37fd698e1
snitkdan <snitkdan@uw.edu>
2016-08-10 10:39:44 -0700

    Added author FK to Book

--- ML7/models.py
+++ ML7/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length = 100)
+    author = models.ForeignKey(Author)
     pub_date = models.DateField()
     publisher = models.CharField(max_length = 100)
     summary = models.CharField(max_length = 1000)
