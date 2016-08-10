76a8b8be2da4f3f2d719c55b7604202517933a91
snitkdan <snitkdan@uw.edu>
2016-08-10 10:54:10 -0700

    added __str__ for Author

--- ML7/models.py
+++ ML7/models.py
@@ -17,5 +17,8 @@ class Author(models.Model):
     fname = models.CharField(max_length = 50)
     lname = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.fname + " " + self.lname
+
 
 
