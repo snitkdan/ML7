bce146e44879edb8e666aee31ef443467591a441
snitkdan <snitkdan@uw.edu>
2016-08-10 10:40:58 -0700

    Removed Author

--- ML7/models.py
+++ ML7/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length = 100)
-    author = models.ForeignKey(Author)
     pub_date = models.DateField()
     publisher = models.CharField(max_length = 100)
     summary = models.CharField(max_length = 1000)
