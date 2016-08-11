88262119db2e1ad66499a91be7a2ef3fe0c16f64
snitkdan <snitkdan@uw.edu>
2016-08-11 10:50:58 -0700

    Try mixins for BookList

--- ML7/views.py
+++ ML7/views.py
@@ -2,45 +2,25 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 
-
-
-# Lists all stocks or create a new one
+# Lists all books or create a new one
 # books/
-class BookList(APIView):
+class BookList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-    def get(self, request):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many = True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = BookSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_created)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_Request)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+class BookDetail(generics.RetrieveAPIView):
 
-    def get_object(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        book = self.get_object(pk)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
-
-    def delete(self, request, pk, format=None):
-        book = self.get_object(pk)
-        book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
