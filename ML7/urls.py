1a17187ab232d3f734fa5a7ce4679a6eb44dc260
snitkdan <snitkdan@uw.edu>
2016-08-11 10:22:02 -0700

    Update API views with details page

--- ML7/urls.py
+++ ML7/urls.py
@@ -4,7 +4,7 @@ from ML7 import views
 
 urlpatterns = [
     url(r'^books/$', views.BookList.as_view(), name='book-list'),
-    #url(r'^books/(?P<pk>[0-9]+)/$', views.Book_detail.as_view(), name='Book_detail'),
+    url(r'^books/(?P<pk>[0-9]+)/$', views.BookDetail.as_view(), name='Book_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- ML7/views.py
+++ ML7/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import get_list_or_404
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
+
+
 
 # Lists all stocks or create a new one
 # books/
@@ -14,9 +17,30 @@ class BookList(APIView):
         serializer = BookSerializer(books, many = True)
         return Response(serializer.data)
 
-    def post(self):
-        pass
+    def post(self, request, format=None):
+        serializer = BookSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_created)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_Request)
 
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    def get_object(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
