8fb0906704798bab83cbb1a4f01022486015263a
snitkdan <snitkdan@uw.edu>
2016-08-11 10:39:22 -0700

    Fix urls

--- ML7/urls.py
+++ ML7/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from ML7 import views
 
 urlpatterns = [
-    url(r'^', views.BookList.as_view(), name='book-list'),
+    url(r'^$', views.BookList.as_view(), name='book-list'),
     url(r'^(?P<pk>[0-9]+)/$', views.BookDetail.as_view(), name='book-detail'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
