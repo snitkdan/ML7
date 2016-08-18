8fd80e542f73ff7ea2e793ba5658cda954769326
snitkdan <snitkdan@uw.edu>
2016-08-17 21:43:49 -0700

    Added API post

--- ML7/views.py
+++ ML7/views.py
@@ -3,14 +3,14 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.throttling import UserRateThrottle
 
+#Default rate limit = 10/m
+
 # Lists all books or create a new one
 # books/
 
-# ratelimit by ip address limiting gets/creates to 5/m
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
 
 # Get a book, update it, or destroy it!
 # books/pk
